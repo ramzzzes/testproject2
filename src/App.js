02f6d5e5fc22db522955dbd2eb4f0c3e7b4ecb63
ramzes <ramaza.giorgi@gmail.com>
2022-03-13 12:57:56 +0400

    made footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+   <Button variant="success">
+        text
+   </Button>
+  );
+}
+
+export default App;
